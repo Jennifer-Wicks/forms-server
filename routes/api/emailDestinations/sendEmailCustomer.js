f4481c1e4fc90b7e32f16adecb2f3cdce1d3bf42
boneswvb <boneswvb@gmail.com>
2022-01-13 13:01:31 +0200

    13-01-2022 display date fixed

--- routes/api/emailDestinations/sendEmailCustomer.js
+++ routes/api/emailDestinations/sendEmailCustomer.js
@@ -46,10 +46,53 @@ router.post("/customer", (req, res) => {
     var newData = []
 
     function insertInfo() {
+      let switchDate = data.arriveday1.slice(5, 7);
+      let month = "";
+
+      switch (switchDate) {
+        case "01":
+          month = "Jan"
+          break;
+        case "02":
+          month = "Feb";
+          break;
+        case "03":
+          month = "Mar";
+          break;
+        case "04":
+          month = "Apr";
+          break;
+        case "05":
+          month = "May";
+          break;
+        case "06":
+          month = "Jun";
+          break;
+        case "07":
+          month = "Jul";
+          break;
+        case "08":
+          month = "Aug";
+          break;
+        case "09":
+          month = "Sep";
+          break;
+        case "10":
+          month = "Oct";
+          break;
+        case "11":
+          month = "Nov";
+          break;
+        case "12":
+          month = "Dec";
+          break;
+        default:
+          month = "Not selected";
+      }
       var wrongDateArr = data.arriveday1.split("-");
       var wrongDateDep = data.departday1.split("-");
-      let changedDateArr = wrongDateArr[2] + "/" + wrongDateArr[1] + "/" + wrongDateArr[0];
-      let changedDateDep = wrongDateDep[2] + "/" + wrongDateDep[1] + "/" + wrongDateDep[0];
+      let changedDateArr = wrongDateArr[2] + " " + month + " " + wrongDateArr[0];
+      let changedDateDep = wrongDateDep[2] + " " + month + " " + wrongDateDep[0];
       data.arriveday1 = changedDateArr;
       data.departday1 = changedDateDep;
 
@@ -74,20 +117,7 @@ router.post("/customer", (req, res) => {
       replyTo: `${data.email}`,
       to: process.env.TOEMAIL, // receiver email,
       subject: `${data.arriveday1} ${data.book === undefined ? data.quote : data.book}  ${data.resort1} \(${data.name} ${data.surname}\)`,
-      html: `${newData.join(' ')}`,
-      // <br />Embedded image: <img src="./public/cid:kasane-location-map.gif"/> <br />Embedded image: <img src="cid:./public/201040.pdf"/>`,
-      // attachments: [
-      //   {
-      //     filename: 'image.png',
-      //     path: './public/kasane-location-map.gif',
-      //     cid: 'kasane-location-map.gif' //same cid value as in the html img src
-      //   },
-      //   {
-      //     filename: '201040.pdf',
-      //     path: './public/201040.pdf',
-      //     cid: 'unique@kreata.ee' //same cid value as in the html img src
-      //   }
-      // ]
+      html: `${newData.join(' ')}`
     };
 
     transport.sendMail(mail, (err, data) => {
